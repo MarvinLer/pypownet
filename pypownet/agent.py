615f623a19895e3ebd66650881da79024cd88291
Marvin Lerousseau <marvin.lerousseau@gmail.com>
2018-08-20 15:41:02 +0200

    adding basic random line status switches in Agent

--- pypownet/agent.py
+++ pypownet/agent.py
@@ -3,8 +3,10 @@ from pypownet.env import RunEnv
 
 
 class Agent(object):
-    def __init__(self):
+    def __init__(self, environment):
         """Initialize a new agent."""
+        self.environment = environment
+        # Do not forget to call super() for the daughter classes
 
     def act(self, observation):
         """Produces an action given an observation of the environment.
@@ -20,3 +22,24 @@ class Agent(object):
         """ Here, the Agent upgrades his policy given the previous observation, its subsequently produced action, and
         the associated reward. This is where the Agent learns. """
         pass
+
+
+# Examples of baselines agents
+import numpy as np
+
+
+class RandomLineSwitch(Agent):
+    def act(self, observation):
+        # Sanity check: an observation is a structured object defined in the environment file.
+        assert isinstance(observation, self.environment.Observation)
+        action_space = self.environment.action_space
+        number_lines = action_space.n_lines
+        length_action = action_space.n
+
+        topological_switches_subaction = np.zeros((length_action - number_lines,))
+        line_switches_subaction = np.zeros((number_lines,))
+        line_switches_subaction[np.random.randint(number_lines)] = 1
+
+        return np.concatenate((topological_switches_subaction, line_switches_subaction))
+
+    # No learning (i.e. self.feed_reward does pass)
