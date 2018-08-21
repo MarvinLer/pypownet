52c68e13b163d8c21289f9045242f6013543328b
Marvin Lerousseau <marvin.lerousseau@gmail.com>
2018-08-21 15:43:04 +0200

    matpower path now managed within package __init__; removes unnecessary __init__ in parent folder

--- setup.py
+++ setup.py
@@ -4,28 +4,28 @@ from setuptools import setup
 import os
 import re
 # Add matpower to octave path
-mp_path_config = 'matpower_path.config'
-if not os.path.exists(mp_path_config):
-    raise FileNotFoundError('The matpower path configuration file is not found at', mp_path_config)
-with open(mp_path_config, 'r') as f:
-    conf_str = f.read()
-# Search for content within the ' symbols
-path = re.search(r'\'(.*?)\'', conf_str)
-if not path:
-    # If not found try with regular quotes
-    path = re.search(r'\"(.*?)\"', conf_str)
-    if not path:
-        raise ValueError('The matpower paht configuration file is badly shaped; please refer to README.md')
-matpower_path = os.path.abspath(path.group(1))
-
-# Check that the matpower path does exist
-if not os.path.exists(matpower_path):
-    raise FileNotFoundError('Matpower folder %s not found')
-
-# Add matpower path to PYTHONPATH
-import sys
-
-sys.path.append(matpower_path)
+# mp_path_config = 'matpower_path.config'
+# if not os.path.exists(mp_path_config):
+#     raise FileNotFoundError('The matpower path configuration file is not found at', mp_path_config)
+# with open(mp_path_config, 'r') as f:
+#     conf_str = f.read()
+# # Search for content within the ' symbols
+# path = re.search(r'\'(.*?)\'', conf_str)
+# if not path:
+#     # If not found try with regular quotes
+#     path = re.search(r'\"(.*?)\"', conf_str)
+#     if not path:
+#         raise ValueError('The matpower paht configuration file is badly shaped; please refer to README.md')
+# matpower_path = os.path.abspath(path.group(1))
+#
+# # Check that the matpower path does exist
+# if not os.path.exists(matpower_path):
+#     raise FileNotFoundError('Matpower folder %s not found')
+#
+# # Add matpower path to PYTHONPATH
+# import sys
+#
+# sys.path.append(matpower_path)
 
 # Parse required libraries file into a list of string
 with open('requirements.txt', 'r') as f:
