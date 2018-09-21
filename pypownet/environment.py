integration of second backend: pypower; added param for loadflow backend in config files; software still loads matlab files with octave; improved imports management for speeding computation; bugfix chronics timesteps

pypownet/environment.py
@@ -5,9 +5,8 @@ __author__ = 'marvinler'
 import numpy as np
 from enum import Enum
 
-import pypownet.game
-import pypownet.grid
-import pypownet.reward_signal
+from pypownet.game import Game, Action
+from pypownet.reward_signal import DefaultRewardSignal
 
 
 class IllegalActionException(Exception):
@@ -55,7 +54,7 @@ class ActionSpace(object):
 
         :return: an instance of Action that is equivalent to an action doing nothing
         """
-        return pypownet.game.Action(prods_switches_subaction=np.zeros(self.prods_switches_subaction_length),
+        return Action(prods_switches_subaction=np.zeros(self.prods_switches_subaction_length),
                                     loads_switches_subaction=np.zeros(self.loads_switches_subaction_length),
                                     lines_or_switches_subaction=np.zeros(self.lines_or_switches_subaction_length),
                                     lines_ex_switches_subaction=np.zeros(self.lines_ex_switches_subaction_length),
@@ -81,7 +80,7 @@ class ActionSpace(object):
         lines_ex_switches_subaction = array[offset:offset + self.lines_ex_switches_subaction_length]
         lines_status_subaction = array[-self.lines_status_subaction_length:]
 
-        return pypownet.game.Action(prods_switches_subaction=prods_switches_subaction,
+        return Action(prods_switches_subaction=prods_switches_subaction,
                                     loads_switches_subaction=loads_switches_subaction,
                                     lines_or_switches_subaction=lines_or_switches_subaction,
                                     lines_ex_switches_subaction=lines_ex_switches_subaction,
@@ -93,7 +92,7 @@ class ActionSpace(object):
             raise ValueError('Expected binary array of length %d, got None' % self.action_length)
 
         # If the input action is not of class Action, try to format it into Action (action must be array-like)
-        if not isinstance(action, pypownet.game.Action):
+        if not isinstance(action, Action):
             try:
                 formatted_action = self.array_to_action(action)
             except ValueError as e:
@@ -468,7 +467,7 @@ class RunEnv(object):
                  game_over_mode='soft', renderer_latency=None):
         """ Instantiate the game Environment based on the specified parameters. """
         # Instantiate game & action space
-        self.game = pypownet.game.Game(parameters_folder=parameters_folder, game_level=game_level,
+        self.game = Game(parameters_folder=parameters_folder, game_level=game_level,
                                        chronic_looping_mode=chronic_looping_mode, chronic_starting_id=start_id,
                                        game_over_mode=game_over_mode, renderer_frame_latency=renderer_latency)
         self.action_space = ActionSpace(*self.game.get_number_elements(),
@@ -482,7 +481,7 @@ class RunEnv(object):
         if self.game.get_custom_reward_signal() is not None:
             self.reward_signal = self.game.get_custom_reward_signal()
         else:
-            self.reward_signal = pypownet.reward_signal.DefaultRewardSignal(initial_topology=self.game.get_initial_topology())
+            self.reward_signal = DefaultRewardSignal(initial_topology=self.game.get_initial_topology())
 
         self.last_rewards = []
 
@@ -521,9 +520,9 @@ class RunEnv(object):
 
         return sum(reward_aslist) if do_sum else reward_aslist
 
-    def reset(self, restart=True):
+    def reset(self):
         # Reset the grid overall topology
-        self.game.reset(restart=restart)
+        self.game.reset()
         return self._get_obs()
 
     def render(self, mode='human', close=False, game_over=False):

pypownet/game.py
@@ -133,6 +133,7 @@ class Game(object):
 
         # Read parameters
         self.__parameters = Parameters(parameters_folder, game_level)
+        loadflow_backend = self.__parameters.get_loadflow_backend()
         self.is_mode_dc = self.__parameters.is_dc_mode()
         self.hard_overflow_coefficient = self.__parameters.get_hard_overflow_coefficient()
         self.n_timesteps_hard_overflow_is_broken = self.__parameters.get_n_timesteps_hard_overflow_is_broken()
@@ -152,14 +153,14 @@ class Game(object):
         self.game_over_mode = game_over_mode
 
         # Seek and load starting reference grid
-        self.grid = pypownet.grid.Grid(src_filename=self.__parameters.get_reference_grid_path(),
+        self.grid = pypownet.grid.Grid(loadflow_backend=loadflow_backend,
+                                       src_filename=self.__parameters.get_reference_grid_path(),
                                        dc_loadflow=self.is_mode_dc,
                                        new_imaps=self.__chronic.get_imaps())
         # Container that counts the consecutive timesteps lines are soft-overflows
         self.n_timesteps_soft_overflowed_lines = np.zeros((self.grid.n_lines,))
 
         # Retrieve all the pertinent values of the chronic
-        self.timesteps_ids = self.__chronic.get_timestep_ids()
         self.current_timestep_id = None
         self.current_date = None
         self.current_timestep_entries = None
@@ -324,15 +325,16 @@ class Game(object):
 
         :return: :raise ValueError: raised in the case where they are no more scenarios available
         """
+        timesteps_ids = self.__chronic.get_timestep_ids()
         # If there are no more timestep to be played, loads next chronic
-        if self.current_timestep_id == self.timesteps_ids[-1]:
+        if self.current_timestep_id == timesteps_ids[-1]:
             self.__chronic = self.get_next_chronic()
 
         # If no timestep injections has been loaded so far, loads the first one
         if self.current_timestep_id is None:
-            next_timestep_id = self.timesteps_ids[0]
+            next_timestep_id = timesteps_ids[0]
         else:  # Otherwise loads the next one in the list of timesteps injections
-            next_timestep_id = self.timesteps_ids[self.timesteps_ids.index(self.current_timestep_id) + 1]
+            next_timestep_id = timesteps_ids[timesteps_ids.index(self.current_timestep_id) + 1]
 
         # If the method is not simulate, decrement the actual timesteps to wait for the crashed lines (real step call)
         if not is_simulation:
@@ -350,7 +352,7 @@ class Game(object):
 
             # Compute loadflow of current grid
             try:
-                self.grid.compute_loadflow(fname_end='_cascading%d.m' % depth)
+                self.grid.compute_loadflow(fname_end='_cascading%d' % depth)
             except pypownet.grid.DivergingLoadflowException as e:
                 e.text += ': cascading emulation of depth %d has diverged' % depth
                 if self.renderer is not None:
@@ -471,29 +473,28 @@ class Game(object):
         # Apply the newly computed destination topology to the grid
         self.grid.set_lines_status(new_lines_service)
 
-    def reset(self, restart):
+        # import pypower.api
+        # import os
+        # pypower.api.savecase(os.path.join('tmp', self.grid.filename[:-2]+'.py'), self.grid.mpc)
+
+    def reset(self):
         """ Resets the game: put the grid topology to the initial one. Besides, if restart is True, then the game will
         load the first set of injections (i)_{t0}, otherwise the next set of injections of the chronics (i)_{t+1}
-
-        :param restart: True to restart the chronic, else pursue with next timestep
         """
         self.reset_grid()
         self.epoch += 1
-        if restart:  # If restart, put current id to None so that load_next will load first timestep
+        # Loads next chronics if game is hardcore mode
+        if self.game_over_mode == 'hard':  # If restart, put current id to None so that load_next will load first timestep
             self.current_timestep_id = None
             self.timestep = 1
-
-        # Loads next chronics if game is hardcore mode
-        if self.game_over_mode == 'hard':
             self.__chronic = self.get_next_chronic()
 
         try:
             self.load_entries_from_next_timestep()
             self._compute_loadflow_cascading()
         # If after reset there is a diverging loadflow, then recall reset w/o penalty (not the player's fault)
-        except pypownet.grid.DivergingLoadflowException as e:
-            self.logger.error(e)
-            self.reset(restart)
+        except pypownet.grid.DivergingLoadflowException:
+            self.reset()
 
     def reset_grid(self):
         """ Reinitialized the grid by applying the initial topology to the current state (topology).
@@ -508,6 +509,10 @@ class Game(object):
         # Reset voltage magnitude and angles: they change when using AC mode
         self.grid.set_voltage_angles(self.initial_voltage_angles)
         self.grid.set_voltage_magnitudes(self.initial_voltage_magnitudes)
+
+        self.grid.mpc = {k: v for k, v in self.grid.mpc.items() if k in ['bus', 'gen', 'branch', 'baseMVA', 'version']}
+
+        #self.grid.set_flows_to_0()
 
     def step(self, action, _is_simulation=False):
         if _is_simulation:

pypownet/grid.py
@@ -5,12 +5,12 @@ __author__ = 'marvinler'
 import os
 import numpy as np
 import copy
-from oct2py import octave
-from oct2py.utils import Oct2PyError
 from pypownet import ARTIFICIAL_NODE_STARTING_STRING
 from pypownet.chronic import TimestepEntries
 import pypownet.environment
 import math
+import logging
+import importlib
 
 
 class DivergingLoadflowException(Exception):
@@ -38,16 +38,37 @@ def compute_flows_a(active, reactive, voltage, are_lines_on):
 
 
 class Grid(object):
-    def __init__(self, src_filename, dc_loadflow, new_imaps, verbose=False):
+    def __init__(self, loadflow_backend, src_filename, dc_loadflow, new_imaps):
         self.filename = src_filename
         self.dc_loadflow = dc_loadflow  # true to compute loadflow with Direct Current model, False for Alternative Cur.
-        self.save_io = True  # True to save files (one pretty-print file and one IEEE) for each matpower loadflow comp.
+        self.save_io = False  # True to save files (one pretty-print file and one IEEE) for each matpower loadflow comp.
         if not os.path.exists('tmp'):
             os.makedirs('tmp')
-        self.verbose = verbose  # True to print some running logs, including cascading failure depth
 
-        # Container output of Matpower usual functions (mpc structure); contains all grid params/values as dic format
-        self.mpc = octave.loadcase(self.filename, verbose=False)
+        from pypownet import configure_matpower
+        # Import for this instance of __class__ an octave instance (even for pypower backend for loading grid)
+        oct2py = importlib.import_module('oct2py')
+        self.octave = getattr(oct2py, 'octave')
+        configure_matpower()  # Will declare path dependencies and such
+
+        # Initialize loadflow backend
+        if loadflow_backend not in ['matpower', 'pypower']:
+            raise ValueError('should not happen')
+        if loadflow_backend == 'matpower':
+            self.Oct2PyError = getattr(importlib.import_module('oct2py.utils'), 'Oct2PyError')
+            self.loadflow_options = self.octave.mpoption('pf.alg', 'FDBX', 'pf.fd.max_it', 25, 'verbose', 0, 'out.all', 0)
+            self.mpc = self.octave.loadcase(self.filename, verbose=False)
+        elif loadflow_backend == 'pypower':
+            try:
+                self.pypower_api = importlib.import_module('pypower.api')
+                self.loadflow_options = self.pypower_api.ppoption(PF_ALG=2, PF_MAX_IT_FD=15, PF_TOL=1e-6, VERBOSE=0, OUT_ALL=0)
+                self.mpc = self.octave.loadcase(self.filename, verbose=False)
+            except:
+                raise
+        else:
+            raise Exception('Should not happen')
+        self.loadflow_backend = loadflow_backend
+
         # Change thermal limits: in IEEE format, they are contaied in 'branch'
         self.thermal_limits = np.asarray(new_imaps)
         self.mpc['branch'][:, 5] = np.asarray(new_imaps)
@@ -74,6 +95,8 @@ class Grid(object):
         self.topology = Topology(prods_nodes=np.zeros((self.n_prods,)), loads_nodes=np.zeros((self.n_loads,)),
                                  lines_or_nodes=np.zeros((self.n_lines,)), lines_ex_nodes=np.zeros((self.n_lines,)),
                                  mapping_array=mapping_permutation)
+
+        self.logger = logging.getLogger('pypownet.' + __file__)
 
     def get_number_elements(self):
         return self.n_prods, self.n_loads, self.n_lines
@@ -172,29 +195,36 @@ class Grid(object):
         return are_isolated_buses[are_loads], are_isolated_buses[are_prods], are_isolated_buses
         #return sum(are_isolated_buses[are_loads]), sum(are_isolated_buses[are_prods]), are_isolated_buses
 
-    def __vanilla_matpower_callback(self, fname_end=None, verbose=False):
+    def __vanilla_matpower_callback(self, fname_end=''):
         """ Performs a plain matpower callback using octave to compute the loadflow of grid mpc (should be mpc format
         from matpower). This function uses default octave mpoption (they control in certain ways how matpower behaves
         for the loadflow computation.
 
-        :param fname_end: path to the output prettyprint file (produced by matpower) or None to not save this file
-        :param verbose: this verbose refers to the matpower verbose: if enabled, will plot the output grid in terminal
         :return: the ouput of matpower (typically mpc structure), and a boolean success of loadflow indicator
         """
-        # Fonction of matpower to compute loadflow
-        matpower_function = octave.rundcpf if self.dc_loadflow else octave.runpf
-
-        mpopt = octave.mpoption('pf.alg', 'FDBX', 'pf.fd.max_it', 25)
-
         if self.save_io:
-            fname_end = '.m' if fname_end is None else fname_end
-            fname = os.path.abspath(os.path.join('tmp', os.path.basename(self.filename)))[:-2] + fname_end
-            pprint = os.path.abspath(os.path.join('tmp', 'pp' + os.path.basename(self.filename)))[:-2] + fname_end
-            output = matpower_function(self.mpc, mpopt, pprint, fname, verbose=verbose)
+            fname_end += '.py' if self.loadflow_backend == 'pypower' else '.m'
+            fname = os.path.abspath(os.path.join('tmp', os.path.basename(self.filename))) + fname_end
+            pprint = os.path.abspath(os.path.join('tmp', 'pp' + os.path.basename(self.filename))) + fname_end
         else:
-            output = matpower_function(self.mpc, mpopt, verbose=verbose)
+            fname, pprint = '', ''
 
-        loadflow_success = output['success']  # 0 = failed, 1 = successed
+        if self.loadflow_backend == 'pypower':
+            function = self.pypower_api.rundcpf if self.dc_loadflow else self.pypower_api.runpf
+            try:
+                output, loadflow_success = function(self.mpc, self.loadflow_options, pprint, fname)
+            except (RuntimeError, RuntimeWarning):
+                raise DivergingLoadflowException(None, 'The grid is not connexe')
+        elif self.loadflow_backend == 'matpower':
+            function = self.octave.rundcpf if self.dc_loadflow else self.octave.runpf
+            try:
+                output = function(self.mpc, self.loadflow_options, pprint, fname)
+                loadflow_success = output['success']
+            except self.Oct2PyError:
+                raise DivergingLoadflowException(None, 'The grid is not connexe')
+        else:
+            raise Exception('Should not happen')
+
         return output, loadflow_success
 
     def compute_loadflow(self, fname_end):
@@ -207,11 +237,10 @@ class Grid(object):
         of grid not connexe, or voltages issues, or angle issues etc).
         """
         self._synchronize_bus_types(self.mpc, self.are_loads, self.new_slack_bus)
-
         try:
             output, loadflow_success = self.__vanilla_matpower_callback(fname_end=fname_end)
-        except Oct2PyError:
-            raise DivergingLoadflowException(None, 'The grid is not connexe')
+        except DivergingLoadflowException as e:  # Propagates error if one was raised
+            raise e
 
         # Save the loadflow output as current grid
         self.mpc = output
@@ -229,17 +258,17 @@ class Grid(object):
 
     def load_timestep_injections(self, timestep_injections, prods_p=None, prods_v=None, loads_p=None, loads_q=None):
         """ Loads a scenario from class Scenario: contains P and V values for prods, and P and Q values for loads. Other
-        timestep entries are loaded using other modules (including pypownet.game).
-        If one of input except TimestepInjections are not None, they are all used for next injections (used in simulate
-        with planned injections).
+    timestep entries are loaded using other modules (including pypownet.game).
+    If one of input except TimestepInjections are not None, they are all used for next injections (used in simulate
+    with planned injections).
 
-        :param timestep_injections: an instance of class Scenario
-        :return: if do_trigger_lf_computation then the result of self.compute_loadflow else nothing
-        """
+    :param timestep_injections: an instance of class Scenario
+    :return: if do_trigger_lf_computation then the result of self.compute_loadflow else nothing
+    """
         assert isinstance(timestep_injections, TimestepEntries), 'Should not happen'
 
         # Change the filename of self to pretty print middle-end created temporary files
-        self.filename = 'scenario%d.m' % (timestep_injections.get_id())
+        self.filename = 'scenario%d.py' % (timestep_injections.get_id())
 
         mpc = self.mpc
         gen = mpc['gen']
@@ -279,14 +308,18 @@ class Grid(object):
     def get_lines_status(self):
         return self.mpc['branch'][:, 10]
 
+    def set_flows_to_0(self):
+        self.mpc['branch'][:, 13:] = 0.
+
     def apply_topology(self, new_topology):
         # Verify new specified topology is of good number of elements and only 0 or 1
         """ Applies a new topology to self. topology should be an instance of class Topology, with computed values to
-        be replaced in self.
+    be replaced in self.
 
-        :param new_topology: an instance of Topology, with destination values for the nodes values/lines service status
-        """
-        cpy_new_topology = copy.deepcopy(new_topology)  # Deepcopy as this function sometimes uses to-be-fixed Topology
+    :param new_topology: an instance of Topology, with destination values for the nodes values/lines service status
+    """
+        cpy_new_topology = copy.deepcopy(
+            new_topology)  # Deepcopy as this function sometimes uses to-be-fixed Topology
         assert cpy_new_topology.get_length() == self.get_topology().get_length(), 'Should not happen'
         assert set(cpy_new_topology.get_zipped()).issubset([0, 1]), 'Should not happen'
 
@@ -349,14 +382,14 @@ class Grid(object):
 
     def compute_topological_mapping_permutation(self):
         """ Computes a permutation that shuffles the construction order of a topology (prods->loads->lines or->lines ex)
-        into a representation where all elements of a substation are consecutives values (same order, but locally).
-        By construction, the topological vector is the concatenation of the subvectors: productions nodes (for each
-        value, on which node, 0 or 1, the prod is wired), loads nodes, lines origin nodes, lines extremity nodes and the
-        lines service status.
+    into a representation where all elements of a substation are consecutives values (same order, but locally).
+    By construction, the topological vector is the concatenation of the subvectors: productions nodes (for each
+    value, on which node, 0 or 1, the prod is wired), loads nodes, lines origin nodes, lines extremity nodes and the
+    lines service status.
 
-        This function should only be called once, at the instanciation of the grid, for it computes the fixed mapping
-        function for the remaining of the game (also fixed along games).
-        """
+    This function should only be called once, at the instanciation of the grid, for it computes the fixed mapping
+    function for the remaining of the game (also fixed along games).
+    """
         # Retrieve the true ids of the productions, loads, lines origin (substation id where the origin of a line is
         # wired), lines extremity
         prods_ids = self.mpc['gen'][:, 0]
@@ -380,7 +413,8 @@ class Grid(object):
             n_lines_ex = (lines_ex_ids == node_id).sum()
             n_elements = n_prods + n_loads + n_lines_or + n_lines_ex
             substations_n_elements.append(n_elements)
-        assert sum(substations_n_elements) == len(prods_ids) + len(loads_ids) + len(lines_or_ids) + len(lines_ex_ids)
+        assert sum(substations_n_elements) == len(prods_ids) + len(loads_ids) + len(lines_or_ids) + len(
+            lines_ex_ids)
         # Based on the number of elements per substations, store the true id of substations with less than 4 elements
         mononode_substations = substations_ids[np.where(np.array(substations_n_elements) < 4)[0]]
 
@@ -468,10 +502,13 @@ class Grid(object):
                                                 active_prods, reactive_prods, voltage_prods, active_flows_origin,
                                                 reactive_flows_origin, voltage_origin, active_flows_extremity,
                                                 reactive_flows_extremity, voltage_extremity, ampere_flows,
-                                                thermal_limits, lines_status, are_isolated_loads, are_isolated_prods,
-                                                substations_ids_loads, substations_ids_prods, substations_ids_lines_or,
+                                                thermal_limits, lines_status, are_isolated_loads,
+                                                are_isolated_prods,
+                                                substations_ids_loads, substations_ids_prods,
+                                                substations_ids_lines_or,
                                                 substations_ids_lines_ex, timesteps_before_lines_reconnectable=None,
-                                                timesteps_before_planned_maintenance=None, planned_active_loads=None,
+                                                timesteps_before_planned_maintenance=None,
+                                                planned_active_loads=None,
                                                 planned_reactive_loads=None, planned_active_productions=None,
                                                 planned_voltage_productions=None, date=None,
                                                 prods_nodes=prods_nodes, loads_nodes=loads_nodes,
@@ -479,10 +516,10 @@ class Grid(object):
 
     def export_lines_capacity_usage(self, safe_mode=False):
         """ Computes and returns the lines capacity usage, i.e. the elementwise division of the flows in Ampere by the
-            lines nominal thermal limit.
+        lines nominal thermal limit.
 
-            :return: a list of size the number of lines of positive values
-            """
+        :return: a list of size the number of lines of positive values
+        """
         mpc = self.mpc
         branch = mpc['branch']
         to_array = lambda array: np.asarray(array)
@@ -500,15 +537,18 @@ class Grid(object):
             dst_fname = os.path.abspath(os.path.join('tmp', 'snapshot_' + os.path.basename(self.filename)))
             if not os.path.exists('tmp'):
                 os.makedirs('tmp')
-        print('Saved snapshot at', dst_fname)
-        return octave.savecase(dst_fname, self.mpc)
+        self.logger.info('Saved snapshot at', dst_fname)
+        if self.loadflow_backend == 'matpower':
+            return self.octave.savecase(dst_fname, self.mpc)
+        elif self.loadflow_backend == 'pypower':
+            return self.pypower_api.savecase(dst_fname, self.mpc)
 
 
 class Topology(object):
     """
-    This class is a container for the topology lists defining the current topological state of a grid. Topology should
-    be manipulated using this class, as it maintains the adopted convention consistently.
-    """
+This class is a container for the topology lists defining the current topological state of a grid. Topology should
+be manipulated using this class, as it maintains the adopted convention consistently.
+"""
 
     def __init__(self, prods_nodes, loads_nodes, lines_or_nodes, lines_ex_nodes, mapping_array):
         self.prods_nodes = prods_nodes