f0cfd2afe00553afcb021e5f60c96b1dd7785ba7
Marvin Lerousseau <marvin.lerousseau@gmail.com>
2019-05-14 21:34:16 +0200

    adding more deepcopies after discarding the loadback loadflow computation in simulate

--- pypownet/game.py
+++ pypownet/game.py
@@ -887,11 +887,20 @@ class Game(object):
         # Copy variables of a step: timestep id, mpc (~grid), topology (stand-alone in self), and lists of overflows
         before_timestep_id = self.current_timestep_id
         before_mpc = copy.deepcopy(self.grid.mpc)
+        before_grid_topology = copy.deepcopy(self.grid.topology)
         before_are_loads = copy.deepcopy(self.grid.are_loads)
         before_n_timesteps_overflowed_lines = copy.deepcopy(self.n_timesteps_soft_overflowed_lines)
+        before_n_timesteps_soft_overflowed_lines = copy.deepcopy(self.n_timesteps_soft_overflowed_lines)
         before_timesteps_before_lines_reconnectable = copy.deepcopy(self.timesteps_before_lines_reconnectable)
         before_timesteps_before_lines_reactionable = copy.deepcopy(self.timesteps_before_lines_reactionable)
         before_timesteps_before_nodes_reactionable = copy.deepcopy(self.timesteps_before_nodes_reactionable)
+        before_current_timestep_entries = copy.deepcopy(self.current_timestep_entries)
+        before_previous_timestep = self.previous_timestep
+        before_previous_date = copy.deepcopy(self.previous_date)
+        before_current_date = copy.deepcopy(self.current_date)
+        before_n_loads_cut = self.n_loads_cut
+        before_n_prods_cut = self.n_prods_cut
+        before_grid_filename = self.grid.filename
 
         # Save grid AC or DC normal mode, and force DC mode for simulate
         before_dc = self.grid.dc_loadflow
@@ -905,6 +914,15 @@ class Game(object):
             self.timesteps_before_lines_reconnectable = before_timesteps_before_lines_reconnectable
             self.timesteps_before_lines_reactionable = before_timesteps_before_lines_reactionable
             self.timesteps_before_nodes_reactionable = before_timesteps_before_nodes_reactionable
+            self.current_timestep_entries = before_current_timestep_entries
+            self.previous_timestep = before_previous_timestep
+            self.previous_date = before_previous_date
+            self.current_date = before_current_date
+            self.n_timesteps_soft_overflowed_lines = before_n_timesteps_soft_overflowed_lines
+            self.grid.topology = before_grid_topology
+            self.n_loads_cut = before_n_loads_cut
+            self.n_prods_cut = before_n_prods_cut
+            self.grid.filename = before_grid_filename
             return
 
         # Step the action
